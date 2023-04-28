2abf40fb8dc5e521c94842fa490dc23a888fde7a
Gianmaria Bajo <mg1979.git@gmail.com>
2023-04-29 00:34:33 +0200

    fix #11: fails with default colorscheme

--- lua/tabline/highlight.lua
+++ lua/tabline/highlight.lua
@@ -35,21 +35,60 @@ end
 --- t.fg = foreground in #xxxxxx notation
 ---@param group string
 ---@return table
-function M.get_hl(group)
-  if not vim.o.termguicolors then
-    local t = vim.api.nvim_get_hl_by_name(group, false)
-    return { fg = t.foreground or NORMAL.fg, bg = t.background or NORMAL.bg }
+if vim.version().api_level > 10 then
+  function M.get_hl(group)
+    if not vim.o.termguicolors then
+      local t = vim.api.nvim_get_hl(0, { name = group, link = true })
+      if t.link then
+        return M.get_hl(t.link)
+      elseif not t.ctermfg and not t.ctermbg then
+        return NORMAL or { fg = 15, bg = 0 }
+      end
+      return { fg = t.ctermfg or NORMAL.fg or 15, bg = t.ctermbg or NORMAL.bg or 0 }
+    end
+    local t = vim.api.nvim_get_hl(0, { name = group, link = true })
+    if t.link then
+      return M.get_hl(t.link)
+    elseif not t.fg and not t.bg then
+      return NORMAL or {
+        fg = "#FFFFFF",
+        bg = "#000000",
+        rgb_fg = { r = 255, g = 255, b = 255},
+        rgb_bg = { r = 0, g = 0, b = 0},
+      }
+    end
+    t.rgb_fg = t.fg and rgb2tbl(t.fg) or NORMAL.rgb_fg
+    t.rgb_bg = t.bg and rgb2tbl(t.bg) or NORMAL.rgb_bg
+    local f, b = t.rgb_fg, t.rgb_bg
+    t.fg = f and string.format(XFMT, f.r, f.g, f.b) or NORMAL.fg
+    t.bg = b and string.format(XFMT, b.r, b.g, b.b) or NORMAL.bg
+    return t
   end
-  local t = vim.api.nvim_get_hl_by_name(group, true)
-  if not t.foreground and not t.background then
-    return NORMAL or t
+else
+  function M.get_hl(group)
+    if not vim.o.termguicolors then
+      local t = vim.api.nvim_get_hl_by_name(group, false)
+      return {
+        fg = t.foreground or (NORMAL or {}).fg or 15,
+        bg = t.background or (NORMAL or {}).bg or 0,
+      }
+    end
+    local t = vim.api.nvim_get_hl_by_name(group, true)
+    if not t.foreground and not t.background then
+      return NORMAL or {
+        fg = "#FFFFFF",
+        bg = "#000000",
+        rgb_fg = { r = 255, g = 255, b = 255},
+        rgb_bg = { r = 0, g = 0, b = 0},
+      }
+    end
+    t.rgb_fg = t.foreground and rgb2tbl(t.foreground) or NORMAL.rgb_fg
+    t.rgb_bg = t.background and rgb2tbl(t.background) or NORMAL.rgb_bg
+    local f, b = t.rgb_fg, t.rgb_bg
+    t.fg = f and string.format(XFMT, f.r, f.g, f.b) or NORMAL.fg
+    t.bg = b and string.format(XFMT, b.r, b.g, b.b) or NORMAL.bg
+    return t
   end
-  t.rgb_fg = t.foreground and rgb2tbl(t.foreground) or NORMAL.rgb_fg
-  t.rgb_bg = t.background and rgb2tbl(t.background) or NORMAL.rgb_bg
-  local f, b = t.rgb_fg, t.rgb_bg
-  t.fg = f and string.format(XFMT, f.r, f.g, f.b) or NORMAL.fg
-  t.bg = b and string.format(XFMT, b.r, b.g, b.b) or NORMAL.bg
-  return t
 end
 
 --- Reset Normal highlight definition.
